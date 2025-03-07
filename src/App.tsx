024f7c7f6935cceaa379e0e752de4ea542eb9761
Sultan A. Dev <sultanabaniks@gmail.com>
2025-03-07 20:09:41 +0100

    feat: add SVG assets and enhance Tailwind CSS configuration with animations and shadows

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      <div className="bg-[rgb(18,18,18)] text-[#fff] h-[100vh] w-[100%] flex flex-col items-center max-900:px-[0px]">
+      <div className="bg-[rgb(18,18,18)] text-[#fff] h-[100vh] w-[100%] flex flex-col items-center">
         <h1 className='text-[55px] mb-[-30px] font-silk'>QR CODE</h1>
         <h1 className='text-[55px] font-silk'>GENERATOR</h1>
         <p className="font-metropolis text-[18px] font-normal mb-[40px]" >Create QR code for your website in seconds</p>
@@ -11,7 +11,7 @@ function App() {
         <div className="input-field">
           <p className="text-[#808080] font-metropolis mb-[3px]">Enter any URL to generate QR Code</p>
           <input type="text" name="" id="" placeholder="Enter your URL" className="bg-[#2A2A2A] px-[30px] rounded-[100px] h-[50px] w-[500px]" />
-          <button className="block px-[30px] rounded-[100px] h-[50px] w-[500px] mt-[30px]">
+          <button className="block px-[30px] rounded-[100px] h-[50px] w-[500px] mt-[30px] hover:bg-[url('/public/icon.svg')] hover:bg-center hover:bg-no-repeat hover:bg-cover">
             Generate QR Code
 
           </button>
@@ -20,7 +20,7 @@ function App() {
 
         <img src="/public/frame.png" alt="" className="mt-[20px]" />
 
-        <footer className="absolute bottom-0 w-[100%] text-center text-[#808080] font-metropolis border-t border-[#808080] py-[10px]">
+        <footer className="absolute bottom-0 w-[100%] text-center text-[#808080] font-metropolis border-t border-[#808080] py-[0px]">
           <p>Generate QR codes quickly and easily</p>
           <p>Free to use  .  No sign up required </p>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,25 @@ export default {
         white2: "#ebebeb",
       },
       fontFamily: {
-        silk: ["Silkscreen", "sans-serif"], // Example: Replace "Inter" with your desired font
+        silk: ["Silkscreen", "sans-serif"], 
         metropolis: ["Metropolis", "serif"],
         mono: ["Fira Code", "monospace"],
-        custom: ["YourCustomFont", "sans-serif"], // Add your custom font here
+        custom: ["YourCustomFont", "sans-serif"], 
       },
       screens: {
         "max-900": { max: "900px" },
+      },
+      keyframes: {
+        move: {
+          "0%, 100%": { transform: "translateY(-5px)" },
+          "50%": { transform: "translateY(5px)" },
+        },
+      },
+      animation: {
+        move: "move 1.5s ease-in-out infinite",
+      },
+      boxShadow: {
+        "yellow-glow": "0px 0px 20px 0px rgba(255, 187, 52, 0.10)",
       },
     },
   },
