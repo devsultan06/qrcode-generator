d54275969338b854b798882a1fa3125347b8ca9d
Sultan A. Dev <sultanabaniks@gmail.com>
2025-03-07 19:29:59 +0100

    refactor: remove App.css and integrate Tailwind CSS for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="bg-[rgb(18,18,18)] text-[#fff] h-[100vh] w-[100%] flex flex-col items-center max-900:px-[0px]">
+        <h1 className='text-[55px] mb-[-30px] font-silk'>QR CODE</h1>
+        <h1 className='text-[55px] font-silk'>GENERATOR</h1>
+        <p className="font-metropolis text-[18px] font-normal mb-[40px]" >Create QR code for your website in seconds</p>
+
+        <div className="input-field">
+          <p className="text-[#808080] font-metropolis mb-[3px]">Enter any URL to generate QR Code</p>
+          <input type="text" name="" id="" placeholder="Enter your URL" className="bg-[#2A2A2A] px-[30px] rounded-[100px] h-[50px] w-[500px]" />
+          <button className="block px-[30px] rounded-[100px] h-[50px] w-[500px] mt-[30px]">
+            Generate QR Code
+
+          </button>
+
+        </div>
+
+        <img src="/public/frame.png" alt="" className="mt-[20px]" />
+
+        <footer className="absolute bottom-0 w-[100%] text-center text-[#808080] font-metropolis border-t border-[#808080] py-[10px]">
+          <p>Generate QR codes quickly and easily</p>
+          <p>Free to use  .  No sign up required </p>
+
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: "#FFD43A",
+        darkblack: "#111111",
+        white2: "#ebebeb",
+      },
+      fontFamily: {
+        silk: ["Silkscreen", "sans-serif"], // Example: Replace "Inter" with your desired font
+        metropolis: ["Metropolis", "serif"],
+        mono: ["Fira Code", "monospace"],
+        custom: ["YourCustomFont", "sans-serif"], // Add your custom font here
+      },
+      screens: {
+        "max-900": { max: "900px" },
+      },
+    },
   },
   plugins: [],
-}
-
+};
